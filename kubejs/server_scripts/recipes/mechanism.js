58e79e439603eb2498a75902359eb3cf58aaa737
minecraft <minecraft@debian>
2024-08-09 17:14:19 +0800

    mechanism registry

--- kubejs/server_scripts/recipes/mechanism.js
+++ kubejs/server_scripts/recipes/mechanism.js
@@ -7,10 +7,22 @@ ServerEvents.recipes(event => {
 		"CPC"
 	],
 	{
-		L: "minecraft:lever",
+		L: "#minecraft:wooden_buttons",
 		B: "kubejs:brass_bolt",
 		G: "create:large_cogwheel",
 		C: "create:cogwheel",
 		P: "tconstruct:pattern"
+	});
+
+	event.shaped(
+	"kubejs:andesite_machine",
+	[
+		" K ",
+		"KAK",
+		" K "
+	],
+	{
+		K: "kubejs:kinetic_mechanism"
+		A: "create:andesite_casing"
 	});
 })
