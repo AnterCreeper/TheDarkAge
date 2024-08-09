717b182f3f5f0c0e5a45a02b6420b324864234d5
minecraft <minecraft@debian>
2024-08-09 10:54:52 +0800

    kubejs item scripts.

---
+++ kubejs/startup_scripts/items/component.js
@@ -0,0 +1,15 @@
+StartupEvents.registry("item", event => {
+	event.create("amethyst_bronze_bolt");
+	event.create("amethyst_bronze_gear");
+	event.create("amethyst_bronze_rod");
+	event.create("brass_bolt");
+	event.create("brass_gear");
+	event.create("diamond_bolt");
+	event.create("diamond_gear");
+	event.create("iron_bolt");
+	event.create("iron_gear");
+	event.create("obsidian_bolt");
+	event.create("obsidian_gear");
+	event.create("steel_bolt");
+	event.create("steel_gear");
+})
---
+++ kubejs/startup_scripts/items/mechanism.js
@@ -0,0 +1,5 @@
+StartupEvents.registry("item", event => {
+	event.create("incomplete_precision_mechanism");
+	event.create("incomplete_kinetic_mechanism");
+	event.create("kinetic_mechanism");
+})
